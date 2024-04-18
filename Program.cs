e2b3240c951e1e6ad966ab7f084faa85d4c8f98d
grisha <grisha.voynich@gmail.com>
2024-04-19 02:43:40 +0700

    add file 4th

--- Program.cs
+++ Program.cs
@@ -17,3 +17,43 @@ class Calculator
 
         Console.WriteLine("Результат: " + result);
     }
+    // Дальше функции идут
+    static double Calculate(double num1, double num2, char operation)
+    {
+        double result = 0;
+        // Через свитчи мы прописываем функции 
+        switch (operation)
+        {
+            // Функция сложения
+            case '+':
+                result = num1 + num2;
+                break;
+            // Функция вычитания
+            case '-':
+                result = num1 - num2;
+                break;
+            // Функция умножения
+            case '*':
+                result = num1 * num2;
+                break;
+            // Функция деления
+            case '/':
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                else
+                {
+                    Console.WriteLine("Деление на ноль невозможно!");
+                    Environment.Exit(0);
+                }
+                break;
+            // Обработка исключений
+            default:
+                Console.WriteLine("Некорректная операция!");
+                Environment.Exit(0);
+                break;
+        }
+        return result;
+    }
+}
