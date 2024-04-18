3a4b74c238a4fbd47874408bca084144c864d3f6
Faith-is <vlad-lazarev228@mail.ru>
2024-04-19 01:56:54 +0700

    First file

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Выберите операцию: +, -, * или /");
+        char operation = Convert.ToChar(Console.ReadLine());
+
+        double result = Calculate(num1, num2, operation);
+
+        Console.WriteLine("Результат: " + result);
+    }
